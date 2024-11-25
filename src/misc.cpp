10d0336e29ea7ba7fbcb6d543856f74a72cff2e2
varunbw <varunbalawade@gmail.com>
2024-11-25 16:05:23 +0000

    Small change

--- src/misc.cpp
+++ src/misc.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 // -- Miscellaneous functions
 /*
-    @brief Frees up the memory of all tree and graph nodes
+    @brief Frees up the memory of all tree and graph nodes.
+
     Containers referred:
+    
     - inline vector<ListNode*> allocatedListNodes;
+    
     - inline vector<TreeNode*> allocatedTreeNodes;
+    
     - inline vector<GraphNode*> allocatedGraphNodes;
 */
 void DeleteAllocatedNodes() {
